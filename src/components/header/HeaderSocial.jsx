530a4fe67d2cfdb115ada9a8ecfc7d20032188dd
NIKHIL RANJAN KUMAR <88980648+niku-19@users.noreply.github.com>
2022-12-03 18:44:43 +0530

    third commit

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -7,16 +7,25 @@ const HeaderSocial = () => {
   return (
     <>
       <div className="header__social">
-        <a href="https://github.com/niku-19" target="_blank">
+        <a
+          href="https://github.com/niku-19"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           {<FaGithub />}
         </a>
         <a
           href="https://www.linkedin.com/in/nikhil-ranjan-kumar-594856193/"
           target="_blank"
+          rel="noopener noreferrer"
         >
           {<BsLinkedin />}
         </a>
-        <a href="https://www.instagram.com/14_nikhil_kumar/" target="_blank">
+        <a
+          href="https://www.instagram.com/14_nikhil_kumar/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           {<FaInstagram />}
         </a>
       </div>

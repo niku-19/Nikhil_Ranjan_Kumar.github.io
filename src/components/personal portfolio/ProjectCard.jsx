b23218fc846e549e473d315ac0d95342f3910ebe
NIKHIL RANJAN KUMAR <88980648+niku-19@users.noreply.github.com>
2022-12-03 17:58:46 +0530

    my first commit

---
+++ src/components/personal portfolio/ProjectCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ProjectDatas from "../Data/ProjectsData";
+
+const ProjectCard = () => {
+  return (
+    <>
+      {ProjectDatas.map((datas) => {
+        return (
+          <div key={datas.id}>
+            <article className="portfolio__items">
+              <div className="portfolio__items-image">
+                <img src={datas.image} alt="" />
+              </div>
+              <h3>{datas.title}</h3>
+              <div className="portfolio__itmes-cta">
+                <a href={datas.link} className="btn">
+                  GithUb
+                </a>
+              </div>
+            </article>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default ProjectCard;

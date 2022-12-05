48dc764917439e0f8ac02fb0f0c7d507900d32a6
NIKHIL RANJAN KUMAR <88980648+niku-19@users.noreply.github.com>
2022-12-05 14:52:25 +0530

    gif commit

--- src/components/Data/ProjectsData.js
+++ src/components/Data/ProjectsData.js
@@ -1,9 +1,9 @@
-import IMG1 from "../../assets/p1.png";
+import IMG1 from "../../assets/p1.gif";
 import IMG2 from "../../assets/p2.png";
 import IMG3 from "../../assets/p3.png";
 import IMG4 from "../../assets/p4.png";
 import IMG5 from "../../assets/p5.png";
-// import IMG6 from "../../assets/p.png";
+import IMG6 from "../../assets/p6.gif";
 
 const ProjectsData = [
   {
@@ -14,34 +14,34 @@ const ProjectsData = [
   },
   {
     id: 2,
-    title: "Project 2",
+    title: "Fudo - The Pizza Resturaunt - React js",
     image: IMG2,
-    link: "",
+    link: "https://github.com/niku-19/College-Project-Food_Ordering_react_app.git",
   },
   {
     id: 3,
-    title: "Project 3",
+    title: "Bankist Ui Template - Vanilla js",
     image: IMG3,
     link: "",
   },
   {
     id: 4,
-    title: "Project 4",
+    title: "Online Bank Dummy - HTML, CSS, JS",
     image: IMG4,
-    link: "",
+    link: "https://github.com/niku-19/bankist-web-app.git",
   },
   {
     id: 5,
-    title: "Project 5",
+    title: "Javascript Game - Guess My Number",
     image: IMG5,
-    link: "",
+    link: "https://github.com/niku-19/game-javascript.git",
   },
-  // {
-  //   id: 6,
-  //   title: "Project 6",
-  //   image: IMG6,
-  //   link: "",
-  // },
+  {
+    id: 6,
+    title: "Food Ordering App - HTML,CSS,JS",
+    image: IMG6,
+    link: "https://github.com/niku-19/college_project.git",
+  },
 ];
 
 export default ProjectsData;

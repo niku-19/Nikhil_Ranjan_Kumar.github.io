914425752149e22670c495e3ca2e2668e4ca6c18
NIKHIL RANJAN KUMAR <88980648+niku-19@users.noreply.github.com>
2023-02-06 02:10:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+//lazy loading..
 const Header = React.lazy(() => import("./components/header/Header"));
 const Nav = React.lazy(() => import("./components/navBar/Nav"));
 const About = React.lazy(() => import("./components/about/About"));
@@ -7,7 +8,7 @@ const Services = React.lazy(() => import("./components/services/Services"));
 const Portfolio = React.lazy(() => import("./components/personal portfolio/PortFolio"));
 const Contact = React.lazy(() => import("./components/contact/Contact"));
 const Footer = React.lazy(() => import("./components/footer/Footer"));
-
+//simple
 // import Nav from "./components/navBar/Nav";
 // import About from "./components/about/About";
 // import Experience from "./components/experience/Experience";
